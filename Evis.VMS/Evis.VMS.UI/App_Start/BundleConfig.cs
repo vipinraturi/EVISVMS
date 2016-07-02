9ec1bbf798188162b843d7d1c475f41c7a599505
Vipin Raturi <vipinr@casemyway.com>
2016-07-02 21:53:38 +0400

    Datepicker plugin added

--- Evis.VMS/Evis.VMS.UI/App_Start/BundleConfig.cs
+++ Evis.VMS/Evis.VMS.UI/App_Start/BundleConfig.cs
@@ -12,6 +12,7 @@ namespace Evis.VMS.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/Content/style").Include(
+                  "~/Content/Styles/jquery-ui.css",
                   "~/Content/Styles/bootstrap.min.css",
                "~/Content/Styles/font-awesome.min.css",
                "~/Content/Styles/bootstrap-progressbar-3.3.4.min.css",
@@ -19,6 +20,7 @@ namespace Evis.VMS.UI
                  ));
 
             bundles.Add(new StyleBundle("~/Content/theme1").Include(
+                  "~/Content/Styles/jquery-ui.css",
                "~/Content/Styles/green.css",
                  "~/Content/Styles/custom.css",
                  "~/Content/Styles/custom_theme1.css",
@@ -29,6 +31,7 @@ namespace Evis.VMS.UI
                  ));
 
             bundles.Add(new StyleBundle("~/Content/theme2").Include(
+                 "~/Content/Styles/jquery-ui.css",
                "~/Content/Styles/green.css",
                 "~/Content/Styles/custom_theme2.css",
                "~/Content/Styles/dropzone.min.css",
@@ -38,6 +41,7 @@ namespace Evis.VMS.UI
                 ));
 
             bundles.Add(new StyleBundle("~/Content/theme3").Include(
+                 "~/Content/Styles/jquery-ui.css",
                "~/Content/Styles/green.css",
                 "~/Content/Styles/custom_theme3.css",
                "~/Content/Styles/dropzone.min.css",
@@ -47,6 +51,7 @@ namespace Evis.VMS.UI
                 ));
 
             bundles.Add(new StyleBundle("~/Content/theme4").Include(
+                 "~/Content/Styles/jquery-ui.css",
                "~/Content/Styles/green.css",
                  "~/Content/Styles/custom_theme4.css",
                "~/Content/Styles/dropzone.min.css",
@@ -55,7 +60,8 @@ namespace Evis.VMS.UI
                "~/Content/Styles/toastr-custom.css"
                 ));
             bundles.Add(new StyleBundle("~/Content/theme5").Include(
-             "~/Content/Styles/green.css",
+               "~/Content/Styles/jquery-ui.css",
+               "~/Content/Styles/green.css",
                "~/Content/Styles/custom_theme5.css",
              "~/Content/Styles/dropzone.min.css",
              "~/Content/Styles/pager.css",
@@ -63,7 +69,8 @@ namespace Evis.VMS.UI
              "~/Content/Styles/toastr-custom.css"
               ));
             bundles.Add(new StyleBundle("~/Content/theme6").Include(
-             "~/Content/Styles/green.css",
+               "~/Content/Styles/jquery-ui.css",
+               "~/Content/Styles/green.css",
                "~/Content/Styles/custom_theme6.css",
              "~/Content/Styles/dropzone.min.css",
              "~/Content/Styles/pager.css",
@@ -71,7 +78,8 @@ namespace Evis.VMS.UI
              "~/Content/Styles/toastr-custom.css"
               ));
             bundles.Add(new StyleBundle("~/Content/theme7").Include(
-             "~/Content/Styles/green.css",
+               "~/Content/Styles/jquery-ui.css",
+               "~/Content/Styles/green.css",
                "~/Content/Styles/custom_theme7.css",
              "~/Content/Styles/dropzone.min.css",
              "~/Content/Styles/pager.css",
@@ -79,28 +87,31 @@ namespace Evis.VMS.UI
              "~/Content/Styles/toastr-custom.css"
               ));
             bundles.Add(new StyleBundle("~/Content/theme8").Include(
-             "~/Content/Styles/green.css",
+               "~/Content/Styles/jquery-ui.css",
+               "~/Content/Styles/green.css",
                "~/Content/Styles/custom_theme8.css",
              "~/Content/Styles/dropzone.min.css",
              "~/Content/Styles/pager.css",
              "~/Content/Styles/toastr.min.css",
              "~/Content/Styles/toastr-custom.css"
+
               ));
 
             var bundle = new ScriptBundle("~/bundles/js")
-                .Include("~/Content/Scripts/plugins/jquery.min.js")
-                .Include("~/Content/Scripts/plugins/bootstrap.min.js")
-                .Include("~/Content/Scripts/plugins/bootstrap-progressbar.min.js")
-                .Include("~/Content/Scripts/plugins/date.js")
-                .Include("~/Content/Scripts/plugins/moment.min.js")
-                .Include("~/Content/Scripts/plugins/daterangepicker.js")
-               .Include("~/Content/Scripts/plugins/validator.min.js")
-                .Include("~/Content/Scripts/plugins/icheck.min.js")
-                 .Include("~/Content/Scripts/plugins/fastclick.js")
-                 .Include("~/Content/Scripts/plugins/dropzone.min.js")
-                 .Include("~/Content/Scripts/plugins/Chart.bundle.js")
-                 .Include("~/Content/Scripts/plugins/googleapi.js")
-                .Include("~/Content/Scripts/plugins/custom.min.js");
+            .Include("~/Content/Scripts/plugins/jquery.min.js")
+            .Include("~/Content/Scripts/plugins/jquery-ui.js")
+            .Include("~/Content/Scripts/plugins/bootstrap.min.js")
+            .Include("~/Content/Scripts/plugins/bootstrap-progressbar.min.js")
+            .Include("~/Content/Scripts/plugins/date.js")
+            .Include("~/Content/Scripts/plugins/moment.min.js")
+            .Include("~/Content/Scripts/plugins/daterangepicker.js")
+            .Include("~/Content/Scripts/plugins/validator.min.js")
+            .Include("~/Content/Scripts/plugins/icheck.min.js")
+            .Include("~/Content/Scripts/plugins/fastclick.js")
+            .Include("~/Content/Scripts/plugins/dropzone.min.js")
+            .Include("~/Content/Scripts/plugins/Chart.bundle.js")
+            .Include("~/Content/Scripts/plugins/googleapi.js")
+            .Include("~/Content/Scripts/plugins/custom.min.js");
 
             bundle.Orderer = new NonOrderingBundleOrderer();
             bundles.Add(bundle);
--- Evis.VMS/Evis.VMS.UI/Content/Scripts/viewModels/VisitorViewModel.js
+++ Evis.VMS/Evis.VMS.UI/Content/Scripts/viewModels/VisitorViewModel.js
@@ -129,5 +129,5 @@ function VisitorViewModel() {
     }
 
     self.GetAllVisitor();
-
+    $('#dateDOB').datepicker();
 }
