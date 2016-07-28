bf883b89cf60a7dde71320b1f89ed8500630fff1
junaidameen23 <junaidameen23@gmail.com>
2016-07-28 10:41:37 +0400

    Implemented the changes as per suraj change request.

--- Evis.VMS/Evis.VMS.Data.Model/Entities/Organization.cs
+++ Evis.VMS/Evis.VMS.Data.Model/Entities/Organization.cs
@@ -19,49 +19,48 @@ namespace Evis.VMS.Data.Model.Entities
         [MaxLength(100)]
         public string CompanyName { get; set; }
 
-        [MaxLength(20)]
-        public string CompanyCode { get; set; }
+        //[MaxLength(20)]
+        //public string CompanyCode { get; set; }
 
-        [MaxLength(20)]
-        public string EIN { get; set; }
-        //Employer Identification Number (EIN) 
-        //are defined as a nine-digit number that the IRS assigns to organizations.
+        //[MaxLength(20)]
+        //public string EIN { get; set; }
+        ////Employer Identification Number (EIN) 
+        ////are defined as a nine-digit number that the IRS assigns to organizations.
 
-        public int? CityId { get; set; }
+        //[Required]
+        //[MaxLength(50)]
+        //public string EmailId { get; set; }
 
-        [Required]
-        [MaxLength(50)]
-        public string EmailId { get; set; }
+        //[Required]
+        //[MaxLength(15)]
+        //public string ContactNumber { get; set; }
 
-        [Required]
-        [MaxLength(15)]
-        public string ContactNumber { get; set; }
+        //[Required]
+        //[MaxLength(500)]
+        //public string ContactAddress { get; set; }
 
-        [Required]
-        [MaxLength(500)]
-        public string ContactAddress { get; set; }
+        //[Required]
+        //[MaxLength(8)]
+        //public string ZipCode { get; set; }
 
-        [Required]
-        [MaxLength(8)]
-        public string ZipCode { get; set; }
-
-        [MaxLength(15)]
-        public string FaxNumber { get; set; }
+        //[MaxLength(15)]
+        //public string FaxNumber { get; set; }
 
         [MaxLength(50)]
         public string WebSite { get; set; }
 
-        [ForeignKey("CityId")]
-        public virtual LookUpValues CityMaster { get; set; }
+        public int? CountryId { get; set; }
+        [ForeignKey("CountryId")]
+        public virtual LookUpValues CountryMaster { get; set; }
 
-        [MaxLength(50)]
-        public string Country { get; set; }
+        //[MaxLength(50)]
+        //public string Country { get; set; }
 
-        [MaxLength(50)]
-        public string State { get; set; }
+        //[MaxLength(50)]
+        //public string State { get; set; }
 
-        [MaxLength(50)]
-        public string City { get; set; }
+        //[MaxLength(50)]
+        //public string City { get; set; }
 
         public string ImagePath { get; set; }
 
