c269eff30483fe0853cdfa2c02f715ed7fc35fff
tintu.tj89@gmail.com <tintu.tj89@gmail.com>
2016-08-22 15:42:18 +0400

    shift details added in dashboard

--- Evis.VMS/Evis.VMS.UI/Content/Scripts/viewModels/DashboardViewModel.js
+++ Evis.VMS/Evis.VMS.UI/Content/Scripts/viewModels/DashboardViewModel.js
@@ -1,5 +1,19 @@
 ﻿function DashboardViewModel() {
-   
+    debugger;
+    var self = this;
+    self.GlobalSearch = ko.observable('');
 
+    self.DataGrid = new RIT.eW.DataGridAjax('/Api/DashBoard/DisplayAllShift', 7);
+    //self.DataGrid.GetData();
+    self.GetAllGateData = function () {
+        self.DataGrid.UpdateSearchParam('?globalSearch=' + JSON.stringify(new Object()));
+        self.DataGrid.GetData();
+    }
+
+    self.GetAllGateData();
+
+    //AjaxCall('/Api/DashBoard/DisplayAllShift', data, 'GET', function (data) {
+
+    //})
     //changetheme('theme7');
 }
