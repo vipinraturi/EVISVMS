b448b9b0e21c518a028298d5ecfe38af13e352af
junaidameen23 <junaidameen23@gmail.com>
2016-08-24 16:26:44 +0400

    Worked on change password issues

--- Evis.VMS/Evis.VMS.UI/Content/Scripts/viewModels/ChangePasswordViewModel.js
+++ Evis.VMS/Evis.VMS.UI/Content/Scripts/viewModels/ChangePasswordViewModel.js
@@ -7,18 +7,40 @@
     self.ConfirmNewPassword = ko.observable('').extend({ required: true, minLength: 8 });
 
     self.SaveChangePassword = function () {
+        if (self.Password() == "") {
+            toastr.clear();
+            toastr.warning("Please enter the current password!");
+            return false;
+        }
+        else if (self.NewPassword() == "") {
+            toastr.clear();
+            toastr.warning("Please enter the new password!");
+            return false;
+        }
+        else if (self.ConfirmNewPassword() == "") {
+            toastr.clear();
+            toastr.warning("Please enter the confirm password!");
+            return false;
+        }
+        else if (self.ConfirmNewPassword() != self.NewPassword()) {
+            toastr.clear();
+            toastr.warning("Confirm password is mismatched with new password!");
+            return false;
+        }
+
         var data = new Object();
         data.Password = self.Password(),
         data.NewPassword = self.NewPassword();
-        //debugger;
         AjaxCall('/Api/User/ChangePassword', data, 'POST', function (result) {
-            //debugger;
             if (result.Success) {
+                toastr.clear();
                 toastr.success(result.Message);
                 ApplyCustomBinding('dashboard');
             }
-            toastr.clear();
-            toastr.warning(result.Message);
+            else if(result.Success == false){
+                toastr.clear();
+                toastr.warning(result.Message);
+            }
         })
     }
 
