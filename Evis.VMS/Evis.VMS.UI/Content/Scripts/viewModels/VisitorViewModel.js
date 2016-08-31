85037c521ba0f179c23d545171f88a89ff06e2a8
tintu.tj89@gmail.com <tintu.tj89@gmail.com>
2016-08-31 10:38:28 +0400

    mobile no validation visitor done

--- Evis.VMS/Evis.VMS.UI/Content/Scripts/viewModels/VisitorViewModel.js
+++ Evis.VMS/Evis.VMS.UI/Content/Scripts/viewModels/VisitorViewModel.js
@@ -3,7 +3,9 @@
     nationality = (nationalityVal != "" ? nationalityVal : undefined);
     typeOfCard = (typeOfCard != "" ? typeOfCard : undefined);
     gender = (gender != "" ? gender : undefined);
-
+    var patterns = {
+        ContactNo: /^\d[\d -]*\d$/,
+    };
     var self = this;
     Id = ko.observable(0);
     VisitorName = ko.observable(visitorName).extend({ required: true });
@@ -13,7 +15,14 @@
     TypeOfCardValue = ko.observable(typeOfCard).extend({ required: true });
     IdNo = ko.observable(idNumber).extend({ required: true });
     Nationality = ko.observable(nationality).extend({ required: true });
-    ContactNo = ko.observable(contactNumber).extend({ required: true });
+    //ContactNo = ko.observable(contactNumber).extend({ required: true });
+    self.ContactNo = ko.observable().extend({
+        required: true,
+        pattern: {
+            message: 'Invalid Mobile No',
+            params: patterns.ContactNo
+        }
+    });
     ContactAddress = ko.observable('');
 
     self.GlobalSearch = ko.observable('');
@@ -23,6 +32,7 @@
     self.Genders = ko.observableArray();
     self.TypeOfCards = ko.observableArray();
     self.Nationalities = ko.observableArray();
+    
     self.errors = ko.validation.group({
         VisitorName: this.VisitorName,
         Gender: this.Gender,
