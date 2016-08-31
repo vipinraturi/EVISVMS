730540081d79eca6da64d45f1234764d325f088e
charlespeter21 <charles.peter21@gmail.com>
2016-08-31 12:40:35 +0400

    Graph responsive and main logo resize

--- Evis.VMS/Evis.VMS.UI/Controllers/ApiControllers/Dashboard/DashboardController.cs
+++ Evis.VMS/Evis.VMS.UI/Controllers/ApiControllers/Dashboard/DashboardController.cs
@@ -46,7 +46,7 @@ namespace Evis.VMS.UI.Controllers.ApiControllers
             // add an item
 
             //q = "[";
-            var currentDate = DateTime.Now.AddDays(1).Date;
+            var currentDate = DateTime.Now.Date;
             var lastDate = DateTime.Now.AddDays(-30).Date;
             List<string> dates = new List<string>();
             
@@ -69,7 +69,7 @@ namespace Evis.VMS.UI.Controllers.ApiControllers
                     mydict.Add(new Tuple<int, string, DateTime>(0, (Convert.ToDateTime(day.Date)).ToString("dd/MM/yyyy"), day));
                 }
             }
-            mydict= mydict.OrderBy(x => x.Item3).ToList();
+            mydict= mydict.OrderByDescending(x => x.Item3).ToList();
             return mydict;
         }
 
