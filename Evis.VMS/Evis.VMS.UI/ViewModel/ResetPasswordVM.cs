5a63582a4c175b4af035dd7e137c8d4af1ee0055
junaidameen23 <junaidameen23@gmail.com>
2016-09-15 15:02:25 +0400

    Bugs Fixed

--- Evis.VMS/Evis.VMS.UI/ViewModel/ResetPasswordVM.cs
+++ Evis.VMS/Evis.VMS.UI/ViewModel/ResetPasswordVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,15 @@ namespace Evis.VMS.UI.ViewModel
     public class ResetPasswordVM
     {
         public string UserId { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(14, ErrorMessage = "Password is having less than 8 characters!", MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public string NewPassword { get; set; }
 
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword")]
         public string ConfirmPassword { get; set; }
     }
 }
