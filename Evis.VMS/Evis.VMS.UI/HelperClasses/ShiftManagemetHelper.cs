ce7b7b061000627f7adf30fb02e055e9bb7558a5
charlespeter21 <charles.peter21@gmail.com>
2016-09-28 10:58:30 +0400

    ShiftManagemet

---
+++ Evis.VMS/Evis.VMS.UI/HelperClasses/ShiftManagemetHelper.cs
@@ -0,0 +1,26 @@
+﻿using Evis.VMS.Business;
+using Evis.VMS.UI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Evis.VMS.UI.HelperClasses
+{
+    public class ShiftManagemetHelper
+    {
+
+        GenericService _genericService = null;
+        public ShiftManagemetHelper()
+        {
+            _genericService = new GenericService();
+        }
+        public IList<ShiftManagementVM> GetShiftData(DateTime fromDate,DateTime toDate, int buldingId, int gateId)
+        {
+            
+
+
+            return null;
+        }
+    }
+}
---
+++ Evis.VMS/Evis.VMS.UI/ViewModel/ShiftManagementVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Evis.VMS.UI.ViewModel
+{
+    public class ShiftManagementVM
+    {
+        public int SecurityId { get; set; }
+        public int BuldingId { get; set; }
+        public int GateId { get; set; }
+        public List<ShiftManagemetDates> ShiftManageDates { get; set; }
+    }
+
+    public class ShiftManagemetDates
+    {
+        public DateTime ShiftDate { get; set; }
+        public List<ShiftDatesShifts> ShiftManage { get; set; }
+
+    }
+
+    public class ShiftDatesShifts
+    {
+        public int ShiftId { get; set; }
+    }
+}
